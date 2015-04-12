3b10b39efc59b5b91b8a042fc3feedc6174ea5d4
luin <i@zihua.li>
2015-04-12 22:25:44 +0800

    Remove unused files

--- redis.js
+++
@@ -1,6 +0,0 @@
-var Redis = require('redis');
-var redis = Redis.createClient();
-
-redis.multi().set('foo', 'bar').get('foo').set('foo').get('foo').exec(function (err, result) {
-  console.log(err, result);
-});
