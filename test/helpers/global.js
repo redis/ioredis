b88d00053000e4d8caf7a5cb66d76fc423c14c91
luin <i@zihua.li>
2015-04-14 00:24:23 +0800

    Add RedisCluster

--- test/helpers/global.js
+++ test/helpers/global.js
@@ -6,11 +6,6 @@ GLOBAL.stub = sinon.stub.bind(sinon);
 var Redis = GLOBAL.Redis = require('../..');
 GLOBAL.MockServer = require('./mock_server');
 
-// beforeEach(function (done) {
-//   this.redis = new Redis();
-//   this.redis.once('connect', done);
-// });
-
 afterEach(function (done) {
   var redis = new Redis();
   redis.flushall(function () {
