238277bda864ee33bd73d53d9a9e0bb5dadfa6ea
luin <i@zihua.li>
2015-04-15 18:40:53 +0800

    Implement cluster

--- test/unit/redis.js
+++ test/unit/redis.js
@@ -3,50 +3,64 @@ describe('Redis', function () {
     it('should parse options correctly', function () {
       stub(Redis.prototype, 'connect');
 
+      var option;
       try {
-        var option1 = getOption();
-        expect(option1).to.have.property('port', 6379);
-        expect(option1).to.have.property('host', 'localhost');
-        expect(option1).to.have.property('family', 4);
+        option = getOption();
+        expect(option).to.have.property('port', 6379);
+        expect(option).to.have.property('host', 'localhost');
+        expect(option).to.have.property('family', 4);
 
-        var option2 = getOption(6380);
-        expect(option2).to.have.property('port', 6380);
-        expect(option2).to.have.property('host', 'localhost');
+        option = getOption(6380);
+        expect(option).to.have.property('port', 6380);
+        expect(option).to.have.property('host', 'localhost');
 
-        var option3 = getOption(6381, '192.168.1.1');
-        expect(option3).to.have.property('port', 6381);
-        expect(option3).to.have.property('host', '192.168.1.1');
+        option = getOption('6380');
+        expect(option).to.have.property('port', 6380);
 
-        var option4 = getOption(6381, '192.168.1.1', {
+        option = getOption(6381, '192.168.1.1');
+        expect(option).to.have.property('port', 6381);
+        expect(option).to.have.property('host', '192.168.1.1');
+
+        option = getOption(6381, '192.168.1.1', {
           password: '123',
           db: 2
         });
-        expect(option4).to.have.property('port', 6381);
-        expect(option4).to.have.property('host', '192.168.1.1');
-        expect(option4).to.have.property('password', '123');
-        expect(option4).to.have.property('db', 2);
+        expect(option).to.have.property('port', 6381);
+        expect(option).to.have.property('host', '192.168.1.1');
+        expect(option).to.have.property('password', '123');
+        expect(option).to.have.property('db', 2);
 
-        var option5 = getOption('redis://:authpassword@127.0.0.1:6380/4');
-        expect(option5).to.have.property('port', 6380);
-        expect(option5).to.have.property('host', '127.0.0.1');
-        expect(option5).to.have.property('password', 'authpassword');
-        expect(option5).to.have.property('db', 4);
+        option = getOption('redis://:authpassword@127.0.0.1:6380/4');
+        expect(option).to.have.property('port', 6380);
+        expect(option).to.have.property('host', '127.0.0.1');
+        expect(option).to.have.property('password', 'authpassword');
+        expect(option).to.have.property('db', 4);
 
-        var option6 = getOption('/tmp/redis.sock');
-        expect(option6).to.have.property('path', '/tmp/redis.sock');
+        option = getOption('/tmp/redis.sock');
+        expect(option).to.have.property('path', '/tmp/redis.sock');
 
-        var option7 = getOption({
+        option = getOption({
           port: 6380,
           host: '192.168.1.1'
         });
-        expect(option7).to.have.property('port', 6380);
-        expect(option7).to.have.property('host', '192.168.1.1');
+        expect(option).to.have.property('port', 6380);
+        expect(option).to.have.property('host', '192.168.1.1');
 
-        var option8 = getOption(6380, {
+        option = getOption({
+          port: '6380'
+        });
+        expect(option).to.have.property('port', 6380);
+
+        option = getOption(6380, {
           host: '192.168.1.1'
         });
-        expect(option8).to.have.property('port', 6380);
-        expect(option8).to.have.property('host', '192.168.1.1');
+        expect(option).to.have.property('port', 6380);
+        expect(option).to.have.property('host', '192.168.1.1');
+
+        option = getOption('6380', {
+          host: '192.168.1.1'
+        });
+        expect(option).to.have.property('port', 6380);
       } catch (err) {
         Redis.prototype.connect.restore();
         throw err;
