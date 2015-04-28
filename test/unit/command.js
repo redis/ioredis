31d689d3030d0f4485c14bc35ce401463e1715b2
luin <i@zihua.li>
2015-04-28 20:06:46 +0800

    Combine Command and ClusterCommand

--- test/unit/command.js
+++ test/unit/command.js
@@ -51,4 +51,26 @@ describe('Command', function () {
       command.resolve(new Buffer('foo'));
     });
   });
+
+  describe('#getKeys()', function () {
+    it('should return keys', function () {
+      expect(getKeys('get', ['foo'])).to.eql(['foo']);
+      expect(getKeys('mget', ['foo', 'bar'])).to.eql(['foo', 'bar']);
+      expect(getKeys('mset', ['foo', 'v1', 'bar', 'v2'])).to.eql(['foo', 'bar']);
+      expect(getKeys('hmset', ['key', 'foo', 'v1', 'bar', 'v2'])).to.eql(['key']);
+      expect(getKeys('blpop', ['key1', 'key2', '17'])).to.eql(['key1', 'key2']);
+      expect(getKeys('evalsha', ['23123', '2', 'foo', 'bar', 'zoo'])).to.eql(['foo', 'bar']);
+      expect(getKeys('evalsha', ['23123', 2, 'foo', 'bar', 'zoo'])).to.eql(['foo', 'bar']);
+      expect(getKeys('sort', ['key'])).to.eql(['key']);
+      expect(getKeys('sort', ['key', 'BY', 'hash:*->field'])).to.eql(['key', 'hash:*']);
+      expect(getKeys('sort', ['key', 'BY', 'hash:*->field', 'LIMIT', 2, 3, 'GET', 'gk', 'GET', '#', 'Get', 'gh->f*', 'DESC', 'ALPHA', 'STORE', 'store'])).to.eql(['key', 'hash:*', 'gk', 'gh->f', 'store']);
+      expect(getKeys('zunionstore', ['out', 2, 'zset1', 'zset2', 'WEIGHTS', 2, 3])).to.eql(['out', 'zset1', 'zset2']);
+      expect(getKeys('zinterstore', ['out', 2, 'zset1', 'zset2', 'WEIGHTS', 2, 3])).to.eql(['out', 'zset1', 'zset2']);
+
+      function getKeys(commandName, args) {
+        var command = new Command(commandName, args);
+        return command.getKeys();
+      }
+    });
+  });
 });
