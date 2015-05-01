8767a3bcf8ba7c3a5100c304f09648fad44ee275
luin <i@zihua.li>
2015-05-01 11:12:08 +0800

    Add tests for maxRedirections option

--- test/functional/cluster.js
+++ test/functional/cluster.js
@@ -157,6 +157,32 @@ describe('cluster', function () {
       });
     });
   });
+
+  describe('maxRedirections', function () {
+    it('should return error when reached max redirection', function (done) {
+      var argvHandler = function (argv) {
+        if (argv[0] === 'cluster' && argv[1] === 'slots') {
+          return [
+            [0, 1, ['127.0.0.1', 30001]],
+            [2, 16383, ['127.0.0.1', 30002]]
+          ];
+        } else if (argv[0] === 'get' && argv[1] === 'foo') {
+          return new Error('ASK ' + utils.calcSlot('foo') + ' 127.0.0.1:30001');
+        }
+      };
+      var node1 = new MockServer(30001, argvHandler);
+      var node2 = new MockServer(30002, argvHandler);
+
+      var cluster = new Redis.Cluster([
+        { host: '127.0.0.1', port: '30001' }
+      ], { maxRedirections: 3 });
+      cluster.get('foo', function (err) {
+        expect(err.message).to.match(/Too many Cluster redirections/);
+        cluster.disconnect();
+        disconnect([node1, node2], done);
+      });
+    });
+  });
 });
 
 function disconnect (clients, callback) {
