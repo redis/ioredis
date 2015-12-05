45364ec93a7951f519846ff8fb0823ec64fc897e
Zihua Li <i@zihua.li>
2015-12-05 13:32:34 +0800

    Add tests for executeFailoverCommands and executeClusterDownCommands

--- test/unit/cluster.js
+++ test/unit/cluster.js
@@ -16,4 +16,38 @@ describe('cluster', function () {
       new Cluster([{}]);
     }).to.not.throw(/startupNodes/);
   });
+
+  describe('#executeFailoverCommands', function () {
+    it('should execute the commands', function (done) {
+      var cluster = {
+        resetFailoverQueue: function () {
+          this.failoverQueue = [];
+        },
+        failoverQueue: []
+      };
+
+      cluster.failoverQueue.push(function () {
+        expect(this.failoverQueue).to.have.length(0);
+        done();
+      }.bind(cluster));
+      Cluster.prototype.executeFailoverCommands.call(cluster);
+    });
+  });
+
+  describe('#executeClusterDownCommands', function () {
+    it('should execute the commands', function (done) {
+      var cluster = {
+        resetClusterDownQueue: function () {
+          this.clusterDownQueue = [];
+        },
+        clusterDownQueue: []
+      };
+
+      cluster.clusterDownQueue.push(function () {
+        expect(this.clusterDownQueue).to.have.length(0);
+        done();
+      }.bind(cluster));
+      Cluster.prototype.executeClusterDownCommands.call(cluster);
+    });
+  });
 });
