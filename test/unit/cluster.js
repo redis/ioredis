d780f3f2446b0fff171ca59eef71ec815ffaf043
luin <i@zihua.li>
2016-01-31 15:30:24 +0800

    test(cluster): add test for passing freezed options
    Related with #238

--- test/unit/cluster.js
+++ test/unit/cluster.js
@@ -3,6 +3,24 @@
 var Cluster = require('../../lib/cluster');
 
 describe('cluster', function () {
+  beforeEach(function () {
+    stub(Cluster.prototype, 'connect', function () {
+      return Promise.resolve();
+    });
+  });
+
+  afterEach(function () {
+    Cluster.prototype.connect.restore();
+  });
+
+  it('should support frozen options', function () {
+    var options = Object.freeze({ maxRedirections: 1000 });
+    var cluster = new Cluster([{ port: 7777 }], options);
+    expect(cluster.options).to.have.property('showFriendlyErrorStack', false);
+    expect(cluster.options).to.have.property('showFriendlyErrorStack', false);
+    expect(cluster.options).to.have.property('readOnly', false);
+  });
+
   it('should throw when startupNodes is not an array or is empty', function () {
     expect(function () {
       new Cluster();
