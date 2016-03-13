9b9267ff61ff499e3b221969cb6d5100bd2cf590
luin <i@zihua.li>
2016-03-13 13:07:54 +0800

    test(cluster): add test for pipeline on TRYAGAIN error

--- test/functional/cluster.js
+++ test/functional/cluster.js
@@ -709,6 +709,33 @@ describe('cluster', function () {
       });
     });
 
+    it('should retry the command on TRYAGAIN', function (done) {
+      var times = 0;
+      var slotTable = [
+        [0, 16383, ['127.0.0.1', 30001]]
+      ];
+      var server = new MockServer(30001, function (argv) {
+        if (argv[0] === 'cluster' && argv[1] === 'slots') {
+          return slotTable;
+        }
+        if (argv[1] === 'foo') {
+          if (times++ < 2) {
+            return new Error('TRYAGAIN Multiple keys request during rehashing of slot');
+          }
+        }
+      });
+
+      var cluster = new Redis.Cluster([
+        { host: '127.0.0.1', port: '30001' }
+      ], { retryDelayOnTryAgain: 1 });
+      cluster.pipeline().get('foo').set('foo', 'bar').exec(function (err, result) {
+        expect(result[0][1]).to.eql('OK');
+        expect(result[1][1]).to.eql('OK');
+        cluster.disconnect();
+        disconnect([server], done);
+      });
+    });
+
     it('should not redirect commands on a non-readonly command is successful', function (done) {
       var slotTable = [
         [0, 12181, ['127.0.0.1', 30001]],
