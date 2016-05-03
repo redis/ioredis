19cd6cd1eb4103099c4e517b7fbdfe24170c4e72
luin <i@zihua.li>
2016-05-03 10:53:03 +0800

    test: add test for dropBufferSupport option

---
+++ test/functional/drop_buffer_support.js
@@ -0,0 +1,85 @@
+'use strict';
+
+describe.only('dropBufferSupport', function () {
+  it('should be disabled by default', function () {
+    var redis = new Redis({ lazyConnect: true });
+    expect(redis.options).to.have.property('dropBufferSupport', false);
+  });
+
+  it('should reject the buffer commands', function (done) {
+    var redis = new Redis({ dropBufferSupport: true });
+    redis.getBuffer('foo', function (err) {
+      expect(err.message).to.match(/Buffer methods are not available/);
+
+      redis.callBuffer('get', 'foo', function (err) {
+        expect(err.message).to.match(/Buffer methods are not available/);
+        redis.disconnect();
+        done();
+      });
+    });
+  });
+
+  it('should reject the custom buffer commands', function (done) {
+    var redis = new Redis({ dropBufferSupport: true });
+    redis.defineCommand('geteval', {
+      numberOfKeys: 0,
+      lua: 'return "string"'
+    });
+    redis.getevalBuffer(function (err) {
+      expect(err.message).to.match(/Buffer methods are not available/);
+      redis.disconnect();
+      done();
+    });
+  });
+
+  it('should set the returnBuffers option to false', function (done) {
+    var redis = new Redis({ dropBufferSupport: true });
+    redis.once('ready', function () {
+      expect(redis.replyParser.options.return_buffers).to.eql(false);
+      redis.disconnect();
+      done();
+    });
+  });
+
+  it('should return strings correctly', function (done) {
+    var redis = new Redis({ dropBufferSupport: true });
+    redis.set('foo', new Buffer('bar'), function (err, res) {
+      expect(err).to.eql(null);
+      expect(res).to.eql('OK');
+      redis.get('foo', function (err, res) {
+        expect(err).to.eql(null);
+        expect(res).to.eql('bar');
+        redis.disconnect();
+        done();
+      });
+    });
+  });
+
+  it('should return strings for custom commands', function (done) {
+    var redis = new Redis({ dropBufferSupport: true });
+    redis.defineCommand('geteval', {
+      numberOfKeys: 0,
+      lua: 'return "string"'
+    });
+    redis.geteval(function (err, res) {
+      expect(err).to.eql(null);
+      expect(res).to.eql('string');
+      redis.disconnect();
+      done();
+    });
+  });
+
+  it('should return strings correctly', function (done) {
+    var redis = new Redis({ dropBufferSupport: true });
+    redis.set('foo', new Buffer('bar'), function (err, res) {
+      expect(err).to.eql(null);
+      expect(res).to.eql('OK');
+      redis.get('foo', function (err, res) {
+        expect(err).to.eql(null);
+        expect(res).to.eql('bar');
+        redis.disconnect();
+        done();
+      });
+    });
+  });
+});
