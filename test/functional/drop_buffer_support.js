beca075a5182cd6095090d3f2a25d40f0a218b11
luin <i@zihua.li>
2016-05-03 20:50:28 +0800

    test: fix the duplicated test

--- test/functional/drop_buffer_support.js
+++ test/functional/drop_buffer_support.js
@@ -6,68 +6,70 @@ describe('dropBufferSupport', function () {
     expect(redis.options).to.have.property('dropBufferSupport', false);
   });
 
-  it('should reject the buffer commands', function (done) {
-    var redis = new Redis({ dropBufferSupport: true });
-    redis.getBuffer('foo', function (err) {
-      expect(err.message).to.match(/Buffer methods are not available/);
+  it('should return strings correctly', function (done) {
+    var redis = new Redis({ dropBufferSupport: false });
+    redis.set('foo', new Buffer('bar'), function (err, res) {
+      expect(err).to.eql(null);
+      expect(res).to.eql('OK');
+      redis.get('foo', function (err, res) {
+        expect(err).to.eql(null);
+        expect(res).to.eql('bar');
+        redis.disconnect();
+        done();
+      });
+    });
+  });
 
-      redis.callBuffer('get', 'foo', function (err) {
+  context('enabled', function () {
+    it('should reject the buffer commands', function (done) {
+      var redis = new Redis({ dropBufferSupport: true });
+      redis.getBuffer('foo', function (err) {
+        expect(err.message).to.match(/Buffer methods are not available/);
+
+        redis.callBuffer('get', 'foo', function (err) {
+          expect(err.message).to.match(/Buffer methods are not available/);
+          redis.disconnect();
+          done();
+        });
+      });
+    });
+
+    it('should reject the custom buffer commands', function (done) {
+      var redis = new Redis({ dropBufferSupport: true });
+      redis.defineCommand('geteval', {
+        numberOfKeys: 0,
+        lua: 'return "string"'
+      });
+      redis.getevalBuffer(function (err) {
         expect(err.message).to.match(/Buffer methods are not available/);
         redis.disconnect();
         done();
       });
     });
-  });
 
-  it('should reject the custom buffer commands', function (done) {
-    var redis = new Redis({ dropBufferSupport: true });
-    redis.defineCommand('geteval', {
-      numberOfKeys: 0,
-      lua: 'return "string"'
-    });
-    redis.getevalBuffer(function (err) {
-      expect(err.message).to.match(/Buffer methods are not available/);
-      redis.disconnect();
-      done();
-    });
-  });
-
-  it('should return strings correctly', function (done) {
-    var redis = new Redis({ dropBufferSupport: true });
-    redis.set('foo', new Buffer('bar'), function (err, res) {
-      expect(err).to.eql(null);
-      expect(res).to.eql('OK');
-      redis.get('foo', function (err, res) {
+    it('should return strings correctly', function (done) {
+      var redis = new Redis({ dropBufferSupport: true });
+      redis.set('foo', new Buffer('bar'), function (err, res) {
         expect(err).to.eql(null);
-        expect(res).to.eql('bar');
-        redis.disconnect();
-        done();
+        expect(res).to.eql('OK');
+        redis.get('foo', function (err, res) {
+          expect(err).to.eql(null);
+          expect(res).to.eql('bar');
+          redis.disconnect();
+          done();
+        });
       });
     });
-  });
 
-  it('should return strings for custom commands', function (done) {
-    var redis = new Redis({ dropBufferSupport: true });
-    redis.defineCommand('geteval', {
-      numberOfKeys: 0,
-      lua: 'return "string"'
-    });
-    redis.geteval(function (err, res) {
-      expect(err).to.eql(null);
-      expect(res).to.eql('string');
-      redis.disconnect();
-      done();
-    });
-  });
-
-  it('should return strings correctly', function (done) {
-    var redis = new Redis({ dropBufferSupport: true });
-    redis.set('foo', new Buffer('bar'), function (err, res) {
-      expect(err).to.eql(null);
-      expect(res).to.eql('OK');
-      redis.get('foo', function (err, res) {
+    it('should return strings for custom commands', function (done) {
+      var redis = new Redis({ dropBufferSupport: true });
+      redis.defineCommand('geteval', {
+        numberOfKeys: 0,
+        lua: 'return "string"'
+      });
+      redis.geteval(function (err, res) {
         expect(err).to.eql(null);
-        expect(res).to.eql('bar');
+        expect(res).to.eql('string');
         redis.disconnect();
         done();
       });
