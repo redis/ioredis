87bfebd75fa8666a78eb136c0f8256e102c60981
Shahar Mor <shaharmor1@gmail.com>
2016-06-01 01:33:56 +0300

    fix transaction with dropBufferSupport:true

--- test/functional/drop_buffer_support.js
+++ test/functional/drop_buffer_support.js
@@ -84,8 +84,23 @@ describe('dropBufferSupport', function () {
         expect(err).to.eql(null);
         expect(res[0][1]).to.eql('OK');
         expect(res[1][1]).to.eql('bar');
+        redis.disconnect();
         done();
       });
+    });
+
+    it('should work with transaction', function (done) {
+      var redis = new Redis({ dropBufferSupport: true });
+      redis.multi()
+        .set('foo', 'bar')
+        .get('foo')
+        .exec(function(err, res) {
+          expect(err).to.eql(null);
+          expect(res[0][1]).to.eql('OK');
+          expect(res[1][1]).to.eql('bar');
+          redis.disconnect();
+          done();
+        });
     });
 
     it('should work with internal select command', function (done) {
@@ -94,6 +109,8 @@ describe('dropBufferSupport', function () {
       redis.set('foo', 'bar', function () {
         check.get('foo', function (err, res) {
           expect(res).to.eql('bar');
+          redis.disconnect();
+          check.disconnect();
           done();
         });
       });
