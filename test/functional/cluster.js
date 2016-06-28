89b62296ba2024d5c5d92730a4ab3ca24aafd244
James Allen <james@sharelatex.com>
2016-06-28 15:22:49 +0100

    Ensure node exists before being redirected via an ASK

--- test/functional/cluster.js
+++ test/functional/cluster.js
@@ -452,6 +452,38 @@ describe('cluster', function () {
         cluster.get('foo');
       });
     });
+
+    it('should be able to redirect a command to a unknown node', function (done) {
+      var asked = false;
+      var slotTable = [
+        [0, 16383, ['127.0.0.1', 30002]]
+      ];
+      var node1 = new MockServer(30001, function (argv) {
+        if (argv[0] === 'get' && argv[1] === 'foo') {
+          expect(asked).to.eql(true);
+          return "bar"
+        } else if (argv[0] === 'asking') {
+          asked = true;
+        }
+      });
+      var node2 = new MockServer(30002, function (argv) {
+        if (argv[0] === 'cluster' && argv[1] === 'slots') {
+          return slotTable;
+        }
+        if (argv[0] === 'get' && argv[1] === 'foo') {
+          return new Error('ASK ' + calculateSlot('foo') + ' 127.0.0.1:30001');
+        }
+      });
+
+      var cluster = new Redis.Cluster([
+        { host: '127.0.0.1', port: '30002' }
+      ]);
+      cluster.get('foo', function (err, res) {
+        expect(res).to.eql('bar');
+        cluster.disconnect();
+        disconnect([node1, node2], done);
+      });
+    });
   });
 
   describe('TRYAGAIN', function () {
