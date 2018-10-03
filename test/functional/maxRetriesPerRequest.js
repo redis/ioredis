69b05adf8f815524b2b9a70dc22116b1ce703527
AVVS <v@makeomatic.ru>
2018-10-03 14:27:01 -0700

    chore: revert prepublishOnly to prepublish

--- test/functional/maxRetriesPerRequest.js
+++ test/functional/maxRetriesPerRequest.js
@@ -1,65 +1,65 @@
 'use strict';
 
-var {MaxRetriesPerRequestError} = require('../../lib/errors')
+var { MaxRetriesPerRequestError } = require('../../lib/errors');
 
 describe('maxRetriesPerRequest', function () {
   it('throw the correct error when reached the limit', function (done) {
     var redis = new Redis(9999, {
       retryStrategy() {
-        return 1
+        return 1;
       }
     });
     redis.get('foo', (err) => {
-      expect(err).instanceOf(MaxRetriesPerRequestError)
-      done()
-    })
-  })
+      expect(err).instanceOf(MaxRetriesPerRequestError);
+      done();
+    });
+  });
 
   it('defaults to max 20 retries', function (done) {
     var redis = new Redis(9999, {
       connectTimeout: 1,
       retryStrategy() {
-        return 1
+        return 1;
       }
     });
     redis.get('foo', () => {
-      expect(redis.retryAttempts).to.eql(21)
+      expect(redis.retryAttempts).to.eql(21);
       redis.get('foo', () => {
-        expect(redis.retryAttempts).to.eql(42)
-        done()
-      })
-    })
+        expect(redis.retryAttempts).to.eql(42);
+        done();
+      });
+    });
   });
 
   it('can be changed', function (done) {
     var redis = new Redis(9999, {
       maxRetriesPerRequest: 1,
       retryStrategy() {
-        return 1
+        return 1;
       }
     });
     redis.get('foo', () => {
-      expect(redis.retryAttempts).to.eql(2)
+      expect(redis.retryAttempts).to.eql(2);
       redis.get('foo', () => {
-        expect(redis.retryAttempts).to.eql(4)
-        done()
-      })
-    })
+        expect(redis.retryAttempts).to.eql(4);
+        done();
+      });
+    });
   });
 
   it('allows 0', function (done) {
     var redis = new Redis(9999, {
       maxRetriesPerRequest: 0,
       retryStrategy() {
-        return 1
+        return 1;
       }
     });
     redis.get('foo', () => {
-      expect(redis.retryAttempts).to.eql(1)
+      expect(redis.retryAttempts).to.eql(1);
       redis.get('foo', () => {
-        expect(redis.retryAttempts).to.eql(2)
-        done()
-      })
-    })
+        expect(redis.retryAttempts).to.eql(2);
+        done();
+      });
+    });
   });
 });
