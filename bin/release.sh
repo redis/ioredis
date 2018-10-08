451d34372f25a36963413a54e2513eb2072f66d8
luin <i@zihua.li>
2018-10-08 13:10:56 +0800

    chore: set bump type explicitly when releasing

--- bin/release.sh
+++ bin/release.sh
@@ -1,7 +1,8 @@
 [ -z "$CONVENTIONAL_GITHUB_RELEASER_TOKEN" ] && echo "Need to set Token" && exit 1;
+[ -z "$1" ] && echo "Need to set version bump type" && exit 1;
 cp package.json _package.json &&
 cp package-lock.json _package-lock.json &&
-bump=`conventional-recommended-bump -p angular` &&
+bump=$1 &&
 echo ${1:-$bump} &&
 npm --no-git-tag-version version ${1:-$bump} &>/dev/null &&
 conventional-changelog -i Changelog.md -s -p angular &&
