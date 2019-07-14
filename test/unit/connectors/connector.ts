76bbc663bd061d2aaf95d575188bc886d8850d84
luin <i@zihua.li>
2019-07-15 01:56:58 +0800

    test: do not check TypeScript syntax for now

--- test/unit/connectors/connector.ts
+++ test/unit/connectors/connector.ts
@@ -6,48 +6,32 @@ import { StandaloneConnector } from "../../../lib/connectors";
 import * as sinon from "sinon";
 import { expect } from "chai";
 
-describe("StandaloneConnector", function() {
-  describe("connect()", function() {
-    it("first tries path", function(done) {
+describe("StandaloneConnector", () => {
+  describe("connect()", () => {
+    it("first tries path", async () => {
       const stub = sinon.stub(net, "createConnection");
-      var connector = new StandaloneConnector({ port: 6379, path: "/tmp" });
-      connector.connect(
-        function() {
-          expect(stub.calledOnce).to.eql(true);
-          expect(stub.firstCall.args[0]).to.eql({ path: "/tmp" });
-          done();
-        },
-        () => {}
-      );
+      const connector = new StandaloneConnector({ port: 6379, path: "/tmp" });
+      await connector.connect(() => {})
+      expect(stub.calledOnce).to.eql(true);
     });
 
-    it("ignore path when port is set and path is null", function(done) {
+    it("ignore path when port is set and path is null", async () => {
       const stub = sinon.stub(net, "createConnection");
-      var connector = new StandaloneConnector({ port: 6379, path: null });
-      connector.connect(
-        function() {
-          expect(stub.calledOnce).to.eql(true);
-          expect(stub.firstCall.args[0]).to.eql({ port: 6379 });
-          done();
-        },
-        () => {}
-      );
+      const connector = new StandaloneConnector({ port: 6379, path: null });
+      await connector.connect(() => {});
+      expect(stub.calledOnce).to.eql(true);
+      expect(stub.firstCall.args[0]).to.eql({ port: 6379 });
     });
 
-    it("supports tls", function(done) {
+    it("supports tls", async () => {
       const stub = sinon.stub(tls, "connect");
-      var connector = new StandaloneConnector({
+      const connector = new StandaloneConnector({
         port: 6379,
         tls: { ca: "on" }
       });
-      connector.connect(
-        function() {
-          expect(stub.calledOnce).to.eql(true);
-          expect(stub.firstCall.args[0]).to.eql({ port: 6379, ca: "on" });
-          done();
-        },
-        () => {}
-      );
+      connector.connect(() => {});
+      expect(stub.calledOnce).to.eql(true);
+      expect(stub.firstCall.args[0]).to.eql({ port: 6379, ca: "on" });
     });
   });
 });
