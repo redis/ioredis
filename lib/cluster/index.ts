256e58703c6850e9f099a180d1847fa719e7b419
Guy Korland <gkorland@gmail.com>
2020-08-12 01:10:41 +0300

    clean types warnings

--- lib/cluster/index.ts
+++ lib/cluster/index.ts
@@ -448,7 +448,7 @@ class Cluster extends EventEmitter {
    */
   private refreshSlotsCache(callback?: CallbackFunction<void>): void {
     if (this.isRefreshing) {
-      if (typeof callback === "function") {
+      if (callback) {
         process.nextTick(callback);
       }
       return;
@@ -458,7 +458,7 @@ class Cluster extends EventEmitter {
     const _this = this;
     const wrapper = function (error?: Error) {
       _this.isRefreshing = false;
-      if (typeof callback === "function") {
+      if (callback) {
         callback(error);
       }
     };
@@ -467,7 +467,7 @@ class Cluster extends EventEmitter {
 
     let lastNodeError = null;
 
-    function tryNode(index) {
+    function tryNode(index: number) {
       if (index === nodes.length) {
         const error = new ClusterAllFailedError(
           "Failed to refresh slots cache.",
@@ -686,7 +686,7 @@ class Cluster extends EventEmitter {
     return command.promise;
   }
 
-  handleError(error, ttl, handlers) {
+  handleError(error: Error, ttl: { value?: any; }, handlers) {
     if (typeof ttl.value === "undefined") {
       ttl.value = this.options.maxRedirections;
     } else {
@@ -727,7 +727,7 @@ class Cluster extends EventEmitter {
     }
   }
 
-  getInfoFromNode(redis, callback) {
+  getInfoFromNode(redis, callback: CallbackFunction<void>) {
     if (!redis) {
       return callback(new Error("Node is disconnected"));
     }
@@ -748,7 +748,7 @@ class Cluster extends EventEmitter {
 
     duplicatedConnection.cluster(
       "slots",
-      timeout((err, result) => {
+      timeout((err: Error, result) => {
         duplicatedConnection.disconnect();
         if (err) {
           return callback(err);
@@ -820,7 +820,7 @@ class Cluster extends EventEmitter {
         return callback();
       }
 
-      let state;
+      let state: string;
       const lines = res.split("\r\n");
       for (let i = 0; i < lines.length; ++i) {
         const parts = lines[i].split(":");
