b131550be5599ae3998d04d47e62e512fec04c9e
Joshua Barnett <joshua@barne.tt>
2020-11-26 17:26:27 +0000

    Update index.ts
    Update other site where setInterval is used in the same way.

--- lib/redis/index.ts
+++ lib/redis/index.ts
@@ -295,7 +295,7 @@ Redis.prototype.connect = function (callback) {
       reject(new Error("Redis is already connecting/connected"));
       return;
     }
-
+    clearInterval(this._addedScriptHashesCleanInterval);
     this._addedScriptHashesCleanInterval = setInterval(() => {
       this._addedScriptHashes = {};
     }, this.options.maxScriptsCachingTime);
