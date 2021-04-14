688e3883e18acbcaeba2aa2906b53a6ce1c198c9
José Pedro Dias <jose.dias@sky.uk>
2021-04-14 15:57:53 +0100

    thought setTimeout returned a number, apparently not

--- lib/command.ts
+++ lib/command.ts
@@ -156,7 +156,7 @@ export default class Command implements ICommand {
   public isCustomCommand = false;
   public inTransaction = false;
   public pipelineIndex?: number;
-  private _commandTimeoutTimer?: number;
+  private _commandTimeoutTimer?: Timeout;
 
   private slot?: number | null;
   private keys?: Array<string | Buffer>;
