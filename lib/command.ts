f73c33ee45140ac18414014a3c1b5db0d40bf98d
José Pedro Dias <jose.dias@sky.uk>
2021-04-15 09:27:22 +0100

    addressing feedback

--- lib/command.ts
+++ lib/command.ts
@@ -156,7 +156,7 @@ export default class Command implements ICommand {
   public isCustomCommand = false;
   public inTransaction = false;
   public pipelineIndex?: number;
-  private _commandTimeoutTimer?: ReturnType<typeof setTimeout>;
+  private _commandTimeoutTimer?: NodeJS.Timeout;
 
   private slot?: number | null;
   private keys?: Array<string | Buffer>;
