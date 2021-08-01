d7477aa5853388b51037210542372131919ddfb2
Tyson Andre <tysonandre775@hotmail.com>
2021-08-01 11:01:25 -0400

    fix(cluster): fix autopipeline with keyPrefix or arg array (#1391)
    Previously, the building of pipelines ignored the key prefix.
    It was possible that two keys, foo and bar, might be set into
    the same pipeline. However, after being prefixed by a configured
    "keyPrefix" value, they may no longer belong to the same
    pipeline.
    
    This led to the error:
    "All keys in the pipeline should belong to the same slots
    allocation group"
    
    Similarly, `args[0]` can be a (possibly empty) array which the Command
    constructor would flatten. Properly compute the first flattened key when
    autopipelining for a Redis.Cluster instance.
    
    Amended version of https://github.com/luin/ioredis/pull/1335/files - see comments on that PR
    
    Closes #1264
    Closes #1248
    Fixes #1392

--- lib/utils/lodash.ts
+++ lib/utils/lodash.ts
@@ -1,6 +1,7 @@
 import defaults = require("lodash.defaults");
 import flatten = require("lodash.flatten");
+import isArguments = require("lodash.isarguments");
 
 export function noop() {}
 
-export { defaults, flatten };
+export { defaults, flatten, isArguments };
