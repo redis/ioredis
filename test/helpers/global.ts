f848cab92e366267fc40c1db524c057421c8bdd4
Tyson Andre <tysonandre775@hotmail.com>
2021-11-22 20:03:53 -0500

    Start testing with Node 16 LTS
    Starting in node 15, if there was no handler for an unhandledRejection,
    the Node.js runtime would automatically throw an error, converting it to
    an uncaughtException.
    
    Previously, mocha was suppressing unhandledRejections such as those from
    redis.connect()

--- test/helpers/global.ts
+++ test/helpers/global.ts
@@ -11,4 +11,20 @@ afterEach(function (done) {
     .exec(done);
 });
 
-console.error = function () {};
+process.on("unhandledRejection", (reason) => {
+  console.log("mocha test saw unexpected unhandledRejection", reason);
+  throw new Error("mocha test saw unexpected unhandledRejection: " + reason);
+});
+
+// Suppress "Unhandled error event" logs from edge cases that are deliberately being tested.
+// Don't suppress other error types, such as errors in typescript files.
+const error = console.error;
+console.error = function (...args) {
+  if (
+    typeof args[0] === "string" &&
+    args[0].indexOf("[ioredis] Unhandled error event") === 0
+  ) {
+    return;
+  }
+  error.call(console, ...args);
+};
