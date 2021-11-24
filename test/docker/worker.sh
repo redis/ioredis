b20e37d22d1091cc5cecf9fe365f68b450cf170a
Tyson Andre <tysonandre775@hotmail.com>
2021-11-24 14:40:57 -0500

    Add dockerized test runner

---
+++ test/docker/worker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -u
+
+# 1. Lint files
+npm run lint || exit 1
+# 2. Build typescript files (needed to run tests)
+npm run build || exit 1
+# 3. Run tests with mock redis server
+redis-server --port 6379 &
+REDIS_PID=$?
+sleep 1
+echo "Started redis as process $REDIS_PID"
+trap "trap - SIGINT SIGTERM EXIT && kill $REDIS_PID" SIGINT SIGTERM EXIT
+npm run test || exit 1
