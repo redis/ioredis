df9330b15e833721830e0cc3ef3173e8a4424932
Zihua Li <i@zihua.li>
2022-02-20 16:17:44 +0800

    chore: upgrade dev dependencies

--- test/functional/string_numbers.ts
+++ test/functional/string_numbers.ts
@@ -5,36 +5,28 @@ const MAX_NUMBER = 9007199254740991; // Number.MAX_SAFE_INTEGER
 
 describe("stringNumbers", function () {
   context("enabled", function () {
-    it("returns numbers as strings", function (done) {
+    it("returns numbers as strings", async () => {
       const redis = new Redis({
         stringNumbers: true,
       });
 
-      let pending = 0;
-
-      redis.set("foo", MAX_NUMBER);
-      redis.incr("foo", check("9007199254740992"));
-      redis.incr("foo", check("9007199254740993"));
-      redis.incr("foo", check("9007199254740994"));
+      await redis.set("foo", MAX_NUMBER);
+      expect(await redis.incr("foo")).to.equal("9007199254740992");
+      expect(await redis.incr("foo")).to.equal("9007199254740993");
+      expect(await redis.incr("foo")).to.equal("9007199254740994");
 
       // also works for small interger
-      redis.set("foo", 123);
-      redis.incr("foo", check("124"));
+      await redis.set("foo", 123);
+      expect(await redis.incr("foo")).to.equal("124");
 
       // and floats
-      redis.set("foo", 123.23);
-      redis.incrbyfloat("foo", 1.2, check("124.43"));
+      await redis.set("foo", 123.23);
+      expect(Number(await redis.incrbyfloat("foo", 1.2))).to.be.within(
+        124.42999,
+        124.430001
+      );
 
-      function check(expected) {
-        pending += 1;
-        return function (err, res) {
-          expect(res).to.eql(expected);
-          if (!--pending) {
-            redis.disconnect();
-            done();
-          }
-        };
-      }
+      redis.disconnect();
     });
   });
 
