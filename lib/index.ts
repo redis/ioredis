07c07f84669527596147eac426ea1ac673a353cd
SCG82 <scg082@gmail.com>
2022-12-23 16:54:08 -0800

    Named export to support ESM imports in Typescript

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,7 @@
 exports = module.exports = require("./Redis").default;
 
 export { default } from "./Redis";
-export type { default as Redis } from "./Redis";
+export { default as Redis } from "./Redis";
 export { default as Cluster } from "./cluster";
 
 /**
