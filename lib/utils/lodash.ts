00a34ee8f523ae76f64fd0f3e736043af56fb3f4
Aras Abbasi <aras.abbasi@googlemail.com>
2025-07-11 08:40:19 +0200

    chore: remove lodash

--- lib/utils/lodash.ts
+++ lib/utils/lodash.ts
@@ -1,6 +1,17 @@
-import defaults = require("lodash.defaults");
-import isArguments = require("lodash.isarguments");
-
 export function noop() {}
 
-export { defaults, isArguments };
+export function defaults(...objects: object[]): object {
+  objects.reverse();
+  Object.assign.call(Object.assign, objects);
+  return objects[0];
+}
+
+export function isArguments(value: any): value is ArrayLike<any> {
+  return (
+    typeof value === "object" &&
+    value !== null &&
+    typeof value.length === "number" &&
+    Object.prototype.toString.call(value) === "[object Arguments]" &&
+    Object.prototype.hasOwnProperty.call(value, "callee")
+  );
+}
