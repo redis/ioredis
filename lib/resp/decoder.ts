5a23ce53fa60b02d4eb3ac8d236227621eabbace
Pavel Pashov <pavel.pashov@redis.com>
2026-06-29 16:28:02 +0300

    chore: fix lint issues in RESP decoder

--- lib/resp/decoder.ts
+++ lib/resp/decoder.ts
@@ -392,12 +392,13 @@ export class Decoder {
             : this.#decodeDoubleDecimal.bind(this, isNegative, 0, integer);
 
         case ASCII.E:
-        case ASCII.e:
+        case ASCII.e: {
           this.#cursor = cursor + 1; // skip E/e
           const i = isNegative ? -integer : integer;
           return this.#cursor < chunk.length
             ? this.#decodeDoubleExponent(i, chunk)
             : this.#decodeDoubleExponent.bind(this, i);
+        }
 
         case ASCII["\r"]:
           this.#cursor = cursor + 2; // skip \r\n
@@ -426,12 +427,13 @@ export class Decoder {
       const byte = chunk[cursor];
       switch (byte) {
         case ASCII.E:
-        case ASCII.e:
+        case ASCII.e: {
           this.#cursor = cursor + 1; // skip E/e
           const d = isNegative ? -double : double;
           return this.#cursor === chunk.length
             ? this.#decodeDoubleExponent.bind(this, d)
             : this.#decodeDoubleExponent(d, chunk);
+        }
 
         case ASCII["\r"]:
           this.#cursor = cursor + 2; // skip \r\n
